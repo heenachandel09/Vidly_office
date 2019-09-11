aaad31754ea69a07b9c7c88d5d61b7f9fb5d1e81
heena.chandel <heena.chandel@AWLIN-DT-11565>
2019-09-11 17:56:28 +0530

    Add project files.

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.ViewModels;
+using Vidly.Models; 
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        [Route("Customer")]
+        public ActionResult RenderCustomer()
+        {
+            var customer = new List<Customer>
+           {
+               new Customer {CustomerName="Yes Bank",Id=1},
+               new Customer {CustomerName="Axis Bank",Id=2 }
+           };
+
+            CustomerListViewModel cust = new CustomerListViewModel
+            {
+                customerviewmodel = customer
+            };
+
+            return View(cust);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = new List<Customer>
+           {
+               new Customer {CustomerName="Yes Bank",Id=1},
+               new Customer {CustomerName="Axis Bank",Id=2 }
+           };
+            Customer custDetail = customer.AsEnumerable().SingleOrDefault(x => x.Id == id);
+
+            //customer = customer.SingleOrDefault(x => x.Id == id);
+           
+            ViewBag.id = id;
+            return View("Details", custDetail);
+        }
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    //plain old CLR object which represents the state and behaviour of our application in terms of its problm domain
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
